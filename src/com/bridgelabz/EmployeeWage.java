1127e00e1b213f6c06d851131a44cd99a7d331da
ipratikner <pratiknerlikar@gmail.com>
2021-10-15 22:58:51 +0530

    Added: checking employee is present or not

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,5 +4,13 @@ public class EmployeeWage {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Program");
+
+        //to check is emp present or not
+        int Is_EmpPresent=1;
+        double empCheck = Math.floor(Math.random() * 10 ) % 2 ;
+        if(empCheck == Is_EmpPresent)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent ");
     }
 }
