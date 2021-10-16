7e263315e745f7f1f289e1fc989e8228fba475f8
ipratikner <pratiknerlikar@gmail.com>
2021-10-16 16:43:44 +0530

    Added: checking employee is part time or full time

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,24 +3,31 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static void main(String[] args) {
 
-        //calculate daily wages
-
-        //constants
-        int IS_EMP_PRESENT=1;
+        //Constatnts
+        int IS_PART_TIME=1;
+        int IS_FULL_TIME=2;
         int EMP_RATE_PER_HOUR =20;
 
-        //Variable
+        //Variables
         int empHrs =0;
         int empWage =0;
 
-        //computation
-        double empCheck = Math.floor(Math.random() * 10 ) % 2 ;
-        if(empCheck == IS_EMP_PRESENT)
-            empHrs =8;
-        else
-            empHrs =0;
-        empWage = empHrs *  EMP_RATE_PER_HOUR;
-        System.out.println("Employee Wage is " +empWage );
-
+        //Computation
+        double empCheck = Math.floor(Math.random() * 10 ) % 3 ;
+        if(empCheck == IS_FULL_TIME) {
+            empHrs = 16;
+            empWage = empHrs *  EMP_RATE_PER_HOUR;
+            System.out.println(" Full Time Employee Wage is " +empWage );
+        }
+        else if(empCheck == IS_PART_TIME) {
+            empHrs = 8;
+            empWage = empHrs *  EMP_RATE_PER_HOUR;
+            System.out.println(" Part Time Employee Wage is " +empWage );
+        }
+        else {
+            empHrs= 0;
+            empWage = empHrs *  EMP_RATE_PER_HOUR;
+            System.out.println("Employee is Absent and Wage is " +empWage );
+        }
     }
 }
